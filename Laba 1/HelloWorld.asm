fe0906124bf9378431fcd1eafd85884e69c13a41
Antoś Jurevič <yurevich-anton@mail.ru>
2018-02-24 01:51:02 +0300

    Laba 1 Hello World!

---
+++ Laba 1/HelloWorld.asm
@@ -0,0 +1,16 @@
+name "hi"
+
+org 100h
+
+jmp start ; go to start
+
+msg: db "Hello World!",0Dh,0Ah,24h ;0Dh \0, 0Ah \n
+
+start:           ;metka start
+     mov dx, msg 
+     mov ah, 09h ;subfunction 9, output of string
+     int 21h     ;output the message
+       
+     mov ah,0    ;exit code 0
+     int 16h     ;exit with waiting
+ret              ;return
